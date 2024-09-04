8c3342d895fb6a8e8cc3b44dc4abd7bf8c93f588
Valentin Schiaffino <valenschg@gmail.com>
2024-09-04 18:45:03 -0300

    fix: delete unused imports

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text, Button } from 'react-native'
-import React, { useState } from 'react'
+import { View } from 'react-native'
+import React from 'react'
 import { Card } from '@/components/Card'
 
 const Contador = () => {
